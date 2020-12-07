7bd79ee0a7aa5970fa34d46eb8a01303e6038425
MarcioBugado <marcioprogramando@gmail.com>
2020-12-07 16:41:05 -0300

    feat:add Google Image Search

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 const robots = {
     state: require('./robots/state'),
     input: require('./robots/input'),
-    text: require('./robots/text')
+    text: require('./robots/text'), 
+    image: require('./robots/image')
 } 
 
 async function start() {
    
     robots.input()
     await robots.text();
+    await robots.image()
 }
 
 start()
